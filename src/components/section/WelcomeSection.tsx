39cb6474b2733e24be9906cb9c28d003b0c5cbe9
dnfebri <dwinofebri@gmail.com>
2023-09-22 13:16:08 +0700

    prettier

--- src/components/section/WelcomeSection.tsx
+++ src/components/section/WelcomeSection.tsx
@@ -13,10 +13,10 @@ export const WelcomeSection = () => {
   };
   return (
     <div className="relative text-white bg-pink-800/80 min-h-screen grid place-items-center items-center bg-cover bg-center bg-[url('/assets/images/bg-welcome.jpg')]">
-      <div className="absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-r from-black via-black/10 to-black" />
-      <div className="absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-black via-black/10 to-black" />
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-black via-black/10 to-black" />
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-black via-black/10 to-black" />
       <form onSubmit={handleSubmit} className="z-10">
-        <div className="grid place-items-center mt-auto  max-w-max mx-auto mb-10">
+        <div className="grid mx-auto mt-auto mb-10 place-items-center max-w-max">
           <label className={`text-3xl font-black ${inter.className}`}>
             Masukan Nama
           </label>
@@ -24,9 +24,9 @@ export const WelcomeSection = () => {
             type="text"
             onChange={(e) => setIsName(e.target.value.toUpperCase())}
             value={isName.toUpperCase()}
-            className="bg-transparent border-b border-secondary focus:border-primary input-reset-css transition-all duration-500 px-4 py-2 text-center"
+            className="px-4 py-2 text-center transition-all duration-500 bg-transparent border-b border-secondary focus:border-primary input-reset-css"
           />
-          <div className="text-center my-4">
+          <div className="my-4 text-center">
             <button
               className={`text-white text-xl btn btn-primary p-0 px-8 capitalize ${inter.className}`}
               type="submit"
